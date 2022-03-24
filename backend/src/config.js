17bd9058c9463cf2ac3d80a65ca971675e34d4ca
FinalTryV78 <102269388+FinalTryV78@users.noreply.github.com>
2022-03-23 21:28:07 -0500

    ayn

--- backend/src/config.js
+++ backend/src/config.js
@@ -8,7 +8,7 @@ const network = NETWORK.eth;
 
 // General metadata for Ethereum
 const namePrefix = "Shroom";
-const description = "Remember to replace this description";
+const description = "Remember to replace with Shrooms this description";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
